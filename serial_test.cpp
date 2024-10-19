e586e68de423c6d66f3c6f52ea84ab8cf7dade0e
Fattis <malte@dellby.eu>
2024-10-19 16:19:50 +0200

    asdjasoipdjsad

--- serial_test.cpp
+++ serial_test.cpp
@@ -3,9 +3,9 @@
   http://www.electronicwings.com
  */
 
-#include <WiringPI/wiringPi/wiringPi.h> /* include wiringPi library */
+#include <WiringPi/wiringPi/wiringPi.h> /* include wiringPi library */
 #include <stdio.h>    
-#include <WiringPI/wiringPi/softPwm.h>  /* include header file for software PWM */
+#include <WiringPi/wiringPi/softPwm.h>  /* include header file for software PWM */
 
 int main(){
 	int PWM_pin = 1;		/* GPIO1 as per WiringPi,GPIO18 as per BCM */
