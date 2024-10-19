6614e5a4cc8002cd93ce40e58c819ec8ecf8c1c9
Fattis <malte@dellby.eu>
2024-10-19 17:37:47 +0200

    motor test

--- serial_test.cpp
+++ serial_test.cpp
@@ -15,19 +15,6 @@ int main(){
 	softPwmCreate(PWM_pin,1,100);	/* set PWM channel along with range*/
 	while (1)
 	  {
-		for (intensity = 0; intensity < 101; intensity++)
-		{
-		  softPwmWrite(PWM_pin, intensity); /* change the value of PWM */
-		  delay (10) ;
-		}
-		delay(1);
-
-		for(intensity = 100; intensity >= 0; intensity--)
-		{
-		  softPwmWrite(PWM_pin, intensity);
-		  delay (10);
-		}
-		delay(1);
-		
+		softPwmWrite(PWM_pin, 100);
 	}
 }
